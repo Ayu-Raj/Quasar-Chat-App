d79057c9bd9545d7298e3401bc254506bc46a7d2
Ayush Raj <ayush.raj@pudle.in>
2020-02-29 23:28:04 +0530

    added pages and their routes

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+    
+
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+  
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  computed: {
+    title() {
+      let currentPath = this.$route.fullPath
+      if(currentPath == '/' ) return 'Chat App'
+      else if(currentPath == '/chat' ) return 'Chat'
+      else if(currentPath == '/auth' ) return 'Authentication'
+    }
+  }
+}
+</script>
