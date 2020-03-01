2ca0a0f00a4548d005c3648971ee49c102c93267
Ayush Raj <ayush.raj@pudle.in>
2020-03-01 12:10:31 +0530

    designed login and register page

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-form @submit="submitForm">
+      <q-input
+      v-if="tab == 'register'"
+      outlined
+      class="q-mb-md"
+      v-model="formData.name"
+      label="Name" />
+      <q-input
+      outlined
+      type="email"
+      class="q-mb-md"
+      v-model="formData.email"
+      label="Email" />
+      <q-input
+      outlined
+      type="password"
+      class="q-mb-md"
+      v-model="formData.password"
+      label="Password" />
+      <div class="row">
+          <q-space />
+      <q-btn
+        color="primary"
+        type="submit"
+        :label="tab"
+        />
+    </div>
+  </q-form>
+</template>
+
+<script>
+export default {
+    props: ['tab'],
+    data() {
+        return {
+            formData: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        submitForm() {
+            if(this.tab == "login"){
+                console.log('Login')
+            }
+            else{
+                console.log('Register')
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
